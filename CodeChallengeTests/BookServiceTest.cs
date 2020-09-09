f4591692b1132618aa9e188899dbb1f453c37d0e
jorgemanzo <jmanzo203689@gmail.com>
2020-09-09 15:03:23 -0700

    Starting test writing

---
+++ CodeChallengeTests/BookServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.ObjectModel;
+using CodeChallenge;
+
+
+namespace CodeChallengeTests
+{
+    [TestClass]
+    public class BookServiceTest
+    {
+        [DataTestMethod]
+        [DataRow("", 0, "", false)]
+        [DataRow(null, -1, null, false)]
+        [DataRow("Brian Kernighan", 272, "The C Programming Language", true)]
+        [DataRow("Gamma, Helm, Johnson, Vlissides", -1, "Design Patterns", false)]
+        public void addNewBookChangesCount(string author, int pageCount, string title, bool expectInserted)
+        {
+            bool inserted = BooksService.addNewBook(author, pageCount, title);
+
+            Assert.AreEqual(expectInserted, inserted);
+        }
+
+    }
+}
---
+++ CodeChallengeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CodeChallengeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeChallengeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e78a127-bb9a-4c40-aa96-76bacf59fbf1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
