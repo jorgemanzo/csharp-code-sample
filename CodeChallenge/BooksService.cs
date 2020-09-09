2610491544ffdaabbf0fac4a67dd45d167acc41d
jorgemanzo <jmanzo203689@gmail.com>
2020-09-09 12:55:06 -0700

    Init commit

---
+++ CodeChallenge/BooksService.cs
@@ -0,0 +1,352 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace CodeChallenge
+{
+
+    /// <summary>
+    /// This class is responsible for processing user data and storing it.
+    /// This class behaves "like" a singleton in that there is one instance
+    /// of this type that is accessible behind static methods.
+    /// 
+    /// This is so the code-behind of a view can access and use this class'
+    /// functionality without having to make an instance and be responsible for it.
+    /// In order to interact with this class, a code-behind should rely on the static
+    /// methods defined at the bottom.
+    /// 
+    /// For storing the data, this class can either "mock" storing the data in
+    /// a collection locally, and will mimick a database. However changes made will
+    /// not persist, and IDs will not to be unique.
+    /// 
+    /// An optional function this class provides is to use a MySQL database. When
+    /// enabled, the class will instead connect to a remote MySQL database and
+    /// create / read / update / delete from that database.
+    /// </summary>
+    class BooksService
+    {
+
+        /// <summary>
+        /// In order to enable database functionality, change the bellow constant
+        /// "USE_DB" to true, and then update the constant "CONNECTION_STRING"
+        /// with the data as it applies to your database.
+        /// </summary>
+        private readonly bool USE_DB = false;
+        private readonly string CONNECTION_STRING = "server=0.0.0.0;user=USER;password=PASSWORD;database=DB";
+
+        private ObservableCollection<Book> inventory;
+        private MySqlConnection mariaDB;
+        private static BooksService instance = null;
+
+        private BooksService()
+        {
+            
+            if (USE_DB)
+            {
+                mariaDB = new MySqlConnection(CONNECTION_STRING);
+                inventory = selectAllFromBooks();
+            }
+            else
+            {
+                inventory = mockInventory();
+            }
+        }
+
+        private ObservableCollection<Book> selectAllFromBooks()
+        {
+
+            ObservableCollection<Book> queryResult = new ObservableCollection<Book>();
+
+            try
+            {
+                mariaDB.Open();
+
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM books;", mariaDB);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    queryResult.Add(new Book()
+                    {
+                        Id          = (int)rdr[0],
+                        Author      = (string)rdr[1],
+                        Title       = (string)rdr[2],
+                        PageCount   = (int)rdr[3]
+                    });
+                }
+                rdr.Close();
+            }
+            catch (MySqlException e)
+            {
+
+                MessageBox.Show(e.Message);
+            }
+            mariaDB.Close();
+
+            return queryResult;
+        }
+
+        private bool deleteByBook(Book toDelete)
+        {
+            try
+            {
+                mariaDB.Open();
+
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM books WHERE book_id = @book_id;", mariaDB);
+                cmd.Parameters.AddWithValue("@book_id", toDelete.Id);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                rdr.Close();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+            mariaDB.Close();
+            return true;
+        }
+
+        private bool insertNewBook(string author, int pageCount, string title)
+        {
+            try
+            {
+                mariaDB.Open();
+
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO books (author, title, page_count) VALUES (@author, @title, @page_count);", mariaDB);
+                cmd.Parameters.AddWithValue("@author", author);
+                cmd.Parameters.AddWithValue("@title", title);
+                cmd.Parameters.AddWithValue("@page_count", pageCount);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                rdr.Close();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.Message);
+                throw;
+            }
+            mariaDB.Close();
+            return true;
+        }
+
+        private bool updateBookById(string author, int id, int pageCount, string title)
+        {
+            try
+            {
+                mariaDB.Open();
+
+                MySqlCommand cmd = new MySqlCommand("UPDATE books SET author = @author, title = @title, page_count = @page_count WHERE book_id = @book_id;", mariaDB);
+                cmd.Parameters.AddWithValue("@author", author);
+                cmd.Parameters.AddWithValue("@book_id", id);
+                cmd.Parameters.AddWithValue("@title", title);
+                cmd.Parameters.AddWithValue("@page_count", pageCount);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                rdr.Close();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.Message);
+                throw;
+            }
+            mariaDB.Close();
+            return true;
+        }
+
+        private ObservableCollection<Book> mockInventory()
+        {
+            ObservableCollection<Book> queryResult = new ObservableCollection<Book>();
+            queryResult.Add(new Book()
+            {
+                Title = "The C Programming Language",
+                Author = "Brian Kernighan",
+                PageCount = 276,
+                Id = 1
+            });
+            queryResult.Add(new Book()
+            {
+                Title = "The Rust Programming Language",
+                Author = "Steve Klabnik",
+                PageCount = 276,
+                Id = 2
+            });
+            return queryResult;
+        }
+
+        private bool mockRemoveBook(Book toRemove)
+        {
+            if (Instance.inventory.Remove(toRemove))
+            {
+                System.Console.WriteLine("Removed");
+                return true;
+            }
+            else
+            {
+                System.Console.WriteLine("Not Found");
+                return false;
+            }
+        }
+
+        private bool mockAddNewBook(string author, int id, int pageCount, string title)
+        {
+            Book newBook = new Book()
+            {
+                Author = author,
+                Title = title,
+                PageCount = pageCount
+            };
+            // Keep the old ID if provided (such as during an update), or 
+            // assign the new book a new ID (may not be unique).
+            newBook.Id = id == 0 ? Instance.inventory.Count + 1 : id;
+            Instance.inventory.Add(newBook);
+            return true;
+        }
+
+        private static BooksService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BooksService();
+                }
+                return instance;
+            }
+        }
+
+        private static bool validateInput(string author, int pageCount, string title)
+        {
+            return !String.IsNullOrWhiteSpace(title) && !String.IsNullOrWhiteSpace(author) && pageCount > 0;
+        }
+
+        /// <summary>
+        /// This class provides the locally stored inventory of books.
+        /// If there is no current instance of the singleton, one will
+        /// be created and will query the data store for records.
+        /// </summary>
+        public static ObservableCollection<Book> getBookInventory()
+        {
+            return Instance.inventory;
+        }
+
+
+        /// <summary>
+        /// This class queries the data store for all records, and replaces
+        /// the locally stored collection with the results.
+        /// </summary>
+        public static ObservableCollection<Book> refreshInventory()
+        {
+            if (Instance.USE_DB)
+            {
+                Instance.inventory = Instance.selectAllFromBooks();
+            }
+            return getBookInventory();
+        }
+
+
+        /// <summary>
+        /// This and the following methods provide create / update / delete
+        /// functionality. If using a database, these changes will be performed
+        /// on the database, otherwise they will be applied to the local data
+        /// store.
+        /// </summary>
+        public static bool removeBook(Book toRemove)
+        {
+            if(Instance.USE_DB)
+            {
+                if(Instance.inventory.Contains(toRemove))
+                {
+                    return Instance.deleteByBook(toRemove);
+                }
+                return false;
+            }
+            else
+            {
+                return Instance.mockRemoveBook(toRemove);
+            }
+        }
+
+
+        /// <summary>
+        /// A code-behind should use this method of adding a new book, as
+        /// this will call the neighboring overloaded method, passing an id of 0
+        /// to indicate that a new ID should be assigned to this book (unlike when
+        /// an update is called and an ID is provided).
+        /// </summary>
+        public static bool addNewBook(string author,  int pageCount, string title)
+        {
+            return addNewBook(author, 0, pageCount, title);
+        }
+
+        private static bool addNewBook(string author, int id, int pageCount, string title)
+        {
+            if (validateInput(author, pageCount, title))
+            {
+                if(Instance.USE_DB)
+                {
+                    return Instance.insertNewBook(author, pageCount, title);
+                }
+                else
+                {
+                    return Instance.mockAddNewBook(author, id, pageCount, title);
+                }
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// When updating a book without a database, the book is first removed from the local
+        /// data store and then re-added with the new information, but the same ID. This is done
+        /// to emulate what would be a database for inventory.
+        /// TODO: Add input checking similar to that of addNewBook()
+        /// </summary>
+        public static bool updateBook(Book toReplace, string author, int id, int pageCount, string title)
+        {
+            if(validateInput(author, pageCount, title))
+            {
+                if (Instance.USE_DB)
+                {
+                    return Instance.updateBookById(author, id, pageCount, title);
+                }
+                else if (removeBook(toReplace))
+                {
+                    return addNewBook(author, id, pageCount, title);
+                }
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// Locally filter the inventory by a book's title using LINQ.
+        /// </summary>
+        public static ObservableCollection<Book> getBooksByTitle(string title)
+        {
+            if(!string.IsNullOrWhiteSpace(title))
+            {
+                ObservableCollection<Book> result = new ObservableCollection<Book>();
+                title = title.ToLower();
+                var booksQuery =
+                    from book in Instance.inventory.ToList<Book>()
+                    where book.Title.ToLower().Contains(title)
+                    || book.Title.ToLower().StartsWith(title)
+                    || book.Title.ToLower().EndsWith(title)
+                    select book;
+
+                foreach (Book book in booksQuery)
+                {
+                    result.Add(book);
+                }
+                return result;
+            }
+            else
+            {
+                return getBookInventory();
+            }
+        }
+    }
+}
---
+++ CodeChallenge/MainWindow.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace CodeChallenge
+{
+
+    /// <summary>
+    /// In order to support data binding between the XAML view and this code-behind,
+    /// Dependency Properties are useful here as they extend the regular functionality
+    /// of a regular class property to support binding (among other things).
+    /// 
+    /// By using Dependency Properties, the view can bind to these properties and update
+    /// its presentation of the data when the data changes. Now we can have a code-behind
+    /// whose responsibility is just to set the data that the view will present.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<Book> Books
+        {
+            get { return (ObservableCollection<Book>)GetValue(BooksProperty); }
+            set { SetValue(BooksProperty, value); }
+        }
+        public static readonly DependencyProperty BooksProperty =
+            DependencyProperty.Register("Books", typeof(ObservableCollection<Book>), typeof(MainWindow), new PropertyMetadata(new ObservableCollection<Book>()));
+
+        public Book SelectedBook
+        {
+            get { return (Book)GetValue(SelectedBookProperty); }
+            set { SetValue(SelectedBookProperty, value); }
+        }
+        public static readonly DependencyProperty SelectedBookProperty =
+            DependencyProperty.Register("SelectedBook", typeof(Book), typeof(MainWindow), new PropertyMetadata(new Book()));
+
+
+
+        public string NewBookTitle
+        {
+            get { return (string)GetValue(NewBookTitleProperty); }
+            set { SetValue(NewBookTitleProperty, value); }
+        }
+        public static readonly DependencyProperty NewBookTitleProperty =
+            DependencyProperty.Register("NewBookTitle", typeof(string), typeof(MainWindow), new PropertyMetadata("New Book Title"));
+
+
+
+        public string NewBookAuthor
+        {
+            get { return (string)GetValue(NewBookAuthorProperty); }
+            set { SetValue(NewBookAuthorProperty, value); }
+        }
+        public static readonly DependencyProperty NewBookAuthorProperty =
+            DependencyProperty.Register("NewBookAuthor", typeof(string), typeof(MainWindow), new PropertyMetadata("New Book Author"));
+
+
+
+        public int NewBookPageCount
+        {
+            get { return (int)GetValue(NewBookPageCountProperty); }
+            set { SetValue(NewBookPageCountProperty, value); }
+        }
+        public static readonly DependencyProperty NewBookPageCountProperty =
+            DependencyProperty.Register("NewBookPageCount", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+
+        public bool IsDeleteEnabled
+        {
+            get { return (bool)GetValue(IsDeleteEnabledProperty); }
+            set { SetValue(IsDeleteEnabledProperty, value); }
+        }
+        public static readonly DependencyProperty IsDeleteEnabledProperty =
+            DependencyProperty.Register("IsDeleteEnabled", typeof(bool), typeof(MainWindow), new PropertyMetadata(true));
+
+
+
+        public bool isUpdateEnabled
+        {
+            get { return (bool)GetValue(isUpdateEnabledProperty); }
+            set { SetValue(isUpdateEnabledProperty, value); }
+        }
+        public static readonly DependencyProperty isUpdateEnabledProperty =
+            DependencyProperty.Register("isUpdateEnabled", typeof(bool), typeof(MainWindow), new PropertyMetadata(true));
+
+
+
+        public string UpdateBookTitle
+        {
+            get { return (string)GetValue(UpdateBookTitleProperty); }
+            set { SetValue(UpdateBookTitleProperty, value); }
+        }
+        public static readonly DependencyProperty UpdateBookTitleProperty =
+            DependencyProperty.Register("UpdateBookTitle", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+
+
+        public string UpdateBookAuthor
+        {
+            get { return (string)GetValue(UpdateBookAuthorProperty); }
+            set { SetValue(UpdateBookAuthorProperty, value); }
+        }
+        public static readonly DependencyProperty UpdateBookAuthorProperty =
+            DependencyProperty.Register("UpdateBookAuthor", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+
+
+        public int UpdateBookPageCount
+        {
+            get { return (int)GetValue(UpdateBookPageCountProperty); }
+            set { SetValue(UpdateBookPageCountProperty, value); }
+        }
+        public static readonly DependencyProperty UpdateBookPageCountProperty =
+            DependencyProperty.Register("UpdateBookPageCount", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+        public int UpdateBookId
+        {
+            get { return (int)GetValue(UpdateBookIdProperty); }
+            set { SetValue(UpdateBookIdProperty, value); }
+        }
+        public static readonly DependencyProperty UpdateBookIdProperty =
+            DependencyProperty.Register("UpdateBookId", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+        public string FilterByTitle
+        {
+            get { return (string)GetValue(FilterByTitleProperty); }
+            set { SetValue(FilterByTitleProperty, value); }
+        }
+        public static readonly DependencyProperty FilterByTitleProperty =
+            DependencyProperty.Register("FilterByTitle", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+            Books = BooksService.getBookInventory();
+        }
+
+
+        /// <summary>
+        /// These remaining methods handle events from the view, and rely on a different class for
+        /// processing the data. Once that class returns results, these methods will update the
+        /// dependency properties above, which will in turn update the data presented to the user.
+        /// </summary>
+        private void RemoveClick(object sender, RoutedEventArgs e)
+        {
+            BooksService.removeBook(SelectedBook);
+            Books = BooksService.refreshInventory();
+            IsDeleteEnabled = isUpdateEnabled = false;
+        }
+
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            BooksService.addNewBook(NewBookAuthor, NewBookPageCount, NewBookTitle);
+            Books = BooksService.refreshInventory();
+            IsDeleteEnabled = isUpdateEnabled = SelectedBook != null;
+        }
+
+        private void RowSelect(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if(SelectedBook != null)
+            {
+                UpdateBookAuthor = SelectedBook.Author;
+                UpdateBookTitle = SelectedBook.Title;
+                UpdateBookPageCount = SelectedBook.PageCount;
+                UpdateBookId = SelectedBook.Id;
+                IsDeleteEnabled = isUpdateEnabled = true;
+            }
+            else
+            {
+                UpdateBookAuthor = string.Empty;
+                UpdateBookTitle = string.Empty;
+                UpdateBookPageCount = 0;
+                UpdateBookId = 0;
+                IsDeleteEnabled = isUpdateEnabled = false;
+            }
+        }
+
+        private void UpdateClick(object sender, RoutedEventArgs e)
+        {
+            if(SelectedBook != null)
+            {
+                bool successfulUpdate = BooksService.updateBook(SelectedBook, UpdateBookAuthor, UpdateBookId, UpdateBookPageCount, UpdateBookTitle);
+                Books = BooksService.refreshInventory();
+                IsDeleteEnabled = isUpdateEnabled = !successfulUpdate;
+            }
+        }
+
+        private void SearchClick(object sender, RoutedEventArgs e)
+        {
+            Books = BooksService.getBooksByTitle(FilterByTitle);
+        }
+    }
+}
